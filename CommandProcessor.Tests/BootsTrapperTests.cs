99055a9a49b904c25cf4245f1f98ddc9ec91348e
Rookian <Rookian@gmx.de>
2012-03-20 21:07:24 +0100

    repair

--- CommandProcessor.Tests/BootsTrapperTests.cs
+++ CommandProcessor.Tests/BootsTrapperTests.cs
@@ -1,26 +1,19 @@
-﻿namespace CommandProcessor.Tests
+﻿using CommandProcessor.Sample.IoC;
+using Machine.Specifications;
+using StructureMap;
+
+namespace CommandProcessor.Tests
 {
-    //[Subject(typeof(BootsTrapper))]
-    //public class When_asking_for_message_processor
-    //{
-    //    private static ICommandProcessor _messageProcessor;
+    [Subject(typeof(BootsTrapper))]
+    public class When_asking_for_message_processor
+    {
+        static ICommandProcessor _messageProcessor;
 
-    //    private Establish context = () => new BootsTrapper();
+        Establish context = () => new BootsTrapper().BootsTrap();
 
-    //    private Because of = () => _messageProcessor = ObjectFactory.GetInstance<ICommandProcessor>();
+        Because of = () => _messageProcessor = ObjectFactory.GetInstance<ICommandProcessor>();
 
-    //    private It should_retrieve_message_processor = () => _messageProcessor.ShouldBeOfType(typeof(CommandProcessor));
-    //}
+        It should_retrieve_message_processor = () => _messageProcessor.ShouldBeOfType(typeof(CommandProcessor));
+    }
 
-    //[Subject(typeof(BootsTrapper))]
-    //public class When_asking_for_command_factory
-    //{
-    //    private static ICommandMessageHandlerFactory _commandMessageHandlerFactory;
-
-    //    private Establish context = () => new BootsTrapper();
-
-    //    private Because of = () => _commandMessageHandlerFactory = ObjectFactory.GetInstance<ICommandMessageHandlerFactory>();
-
-    //    private It should_retrieve_command_factory = () => _commandMessageHandlerFactory.ShouldBeOfType(typeof(CommandMessageHandlerFactory));
-    //}
 }
